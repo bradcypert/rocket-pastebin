1590ca36060ea4195e3a6ef4ab87f9cc69121ed3
Brad Cypert <brad.cypert@gmail.com>
2022-10-06 22:05:56 -0400

    initial commit from the tutorial

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+#[macro_use] extern crate rocket;
+
+mod paste_id;
+
+use paste_id::PasteId;
+use rocket::tokio::fs::File;
+use rocket::data::{Data, ToByteUnit};
+use rocket::http::uri::Absolute;
+
+// TODO: Env vars
+const ID_LENGTH: usize = 3;
+const HOST: Absolute<'static> = uri!("http://localhost:8000");
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, retrieve, upload])
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "
+    USAGE
+
+      POST /
+
+          accepts raw data in the body of the request and responds with a URL of
+          a page containing the body's content
+
+      GET /<id>
+
+          retrieves the content for the paste with id `<id>`
+    "
+}
+
+#[get("/<id>")]
+async fn retrieve(id: PasteId<'_>) -> Option<File> {
+    File::open(id.file_path()).await.ok()
+}
+
+// We derive `UriDisplayPath` for `PasteId` in `paste_id.rs`:
+
+#[post("/", data = "<paste>")]
+async fn upload(paste: Data<'_>) -> std::io::Result<String> {
+    let id = PasteId::new(ID_LENGTH);
+    paste.open(128.kibibytes()).into_file(id.file_path()).await?;
+    Ok(uri!(HOST, retrieve(id)).to_string())
+}
